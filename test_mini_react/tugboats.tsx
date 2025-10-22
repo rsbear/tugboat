08b2b9aefdfd8df7f832181ed5befc113e94bbd6
rsbear <hellorosss@gmail.com>
2025-10-22 15:09:14 -0500

    ignore(test_mini_react): updating deps and test

--- test_mini_react/tugboats.tsx
+++ test_mini_react/tugboats.tsx
@@ -38,7 +38,7 @@ function App() {
   return (
     <div className="bg-gray-800/50 p-6 rounded-lg border border-gray-700 animate-fade-in">
       <h1 className="text-2xl font-bold text-white">
-        deno is dopeeee
+        deno is dopey
       </h1>
       <input
         type="text"
