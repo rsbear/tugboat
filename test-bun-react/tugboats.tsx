04950d5977df2c4456bb235cf779a109915ad1da
rsbear <hellorosss@gmail.com>
2025-10-24 22:24:12 -0500

    delete dead file

--- test-bun-react/tugboats.tsx
+++
@@ -1,9 +0,0 @@
-import React, { useEffect } from "react";
-import ReactDOM from "react-dom/client";
-import App from "./app";
-
-export function tugboatReact(targetElement: HTMLElement) {
-  const root = ReactDOM.createRoot(targetElement);
-  root.render(<App />);
-  return root;
-}
